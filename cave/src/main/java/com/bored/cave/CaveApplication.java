1984fbfd51849938a6f4b87250024235f0434ffb
pujith.kurunji <pujith.kurunji@valtech.com>
2021-10-29 08:36:56 +0530

    Corrupted

--- cave/src/main/java/com/bored/cave/CaveApplication.java
+++ cave/src/main/java/com/bored/cave/CaveApplication.java
@@ -6,8 +6,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CaveApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CaveApplication.class, args);
-	}
 	//comment
 }
